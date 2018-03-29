1cafc1fcc05dfee9ec9054b59c790b99be5374a3
Atizaz Abid <atizaz.abid@emblemtechnologies.com>
2018-03-29 13:01:06 +0500

    registered the model to the admin

--- custom_auth/users/admin.py
+++ custom_auth/users/admin.py
@@ -4,3 +4,35 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+"""Integrate with admin module."""
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+from .models import User,UserProfile,Post
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+admin.site.register(UserProfile)
+admin.site.register(Post)
